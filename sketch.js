06367cea414d189cc8ffebdbbba43fbbe86eab31
reshayadav <70374213+reshayadav@users.noreply.github.com>
2020-11-26 11:04:31 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,53 @@
+//Create variables here
+var dog, happydog;
+var dogImg, happyImg;
+var foodS, foodStock;
+var database;
+function preload()
+{
+  //load images here
+  dogImg = loadImage("images/dogImg.png");
+  happyImg = loadImage("images/dogImg1.png");
+}
+
+function setup() {
+	createCanvas(500 ,500);
+ 
+  dog.addImage(dogImg);
+  database = firebase.database;
+  // foodStock = database.ref("food");
+  // foodStock.on("value", readStock);
+}
+
+
+function draw() {  
+background(46,139,87);
+
+// image(dogImg,200,200,150,140);
+
+
+ if(keyWentDown(UP_ARROW)){
+ writeStock(foodS);
+  dog.addImage(happyImg);
+
+}
+  drawSprites();
+
+  //add styles here
+   textSize(15);
+   fill (255);
+   stroke (2);
+   text("Note:  Press UP_ARROW Key To Feed The Dog", 30,30);
+
+}
+
+function writeStock(x){
+ 
+}
+
+
+function readStock(data){
+  foodS = data.val();
+
+}
+
